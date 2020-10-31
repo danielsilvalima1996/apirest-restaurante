1a0854668c817516dd3d32882fb1f1a067cdf1be
danielsilvalima1996 <dsl15021996@gmail.com>
2020-10-31 08:40:22 -0300

    first commit

---
+++ src/main/java/br/com/restaurante/apirestrestaurante/ApirestRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.apirestrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restaurante/apirestrestaurante/ApirestRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.apirestrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
